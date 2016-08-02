659558d4a374f23f68de69111cf00b32155d42bc
Muhammad Zaheer <smzaheerabbas@gmail.com>
2016-08-02 17:17:38 +0500

    evaluate_mlp without restoring checkpoint

---
+++ evaluate_mlp.py
@@ -0,0 +1,170 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os.path
+import time
+import sys
+
+import numpy as np
+import tensorflow as tf
+
+import mnist
+
+# Basic model parameters as external flags.
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_integer('num_epochs', 1, 'Number of epochs to run trainer.')
+flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')
+flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
+flags.DEFINE_integer('batch_size', 100, 'Batch size.')
+flags.DEFINE_string('train_dir', 'cars_dataset/',
+                    'Directory with the training data.')
+flags.DEFINE_string('checkpoint_dir', 'checkpoints',
+                           """Directory where to read model checkpoints.""")
+# Constants used for dealing with the files, matches convert_to_records.
+TRAIN_FILE = 'train.tfrecords'
+VALIDATION_FILE = 'validation.tfrecords'
+
+def read_and_decode(filename_queue):
+  reader = tf.TFRecordReader()
+  _, serialized_example = reader.read(filename_queue)
+  features = tf.parse_single_example(
+      serialized_example,
+      # Defaults are not specified since both keys are required.
+      features={
+          'image_raw': tf.FixedLenFeature([], tf.string),
+          'label': tf.FixedLenFeature([], tf.int64),
+      })
+
+  # Convert from a scalar string tensor (whose single string has
+  # length mnist.IMAGE_PIXELS) to a uint8 tensor with shape
+  # [mnist.IMAGE_PIXELS].
+  image = tf.decode_raw(features['image_raw'], tf.float32)
+  image.set_shape([3*224*224])
+
+  # OPTIONAL: Could reshape into a 28x28 image and apply distortions
+  # here.  Since we are not applying any distortions in this
+  # example, and the next step expects the image to be flattened
+  # into a vector, we don't bother.
+
+  # Convert from [0, 255] -> [-0.5, 0.5] floats.
+  image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
+
+  # Convert label from a scalar uint8 tensor to an int32 scalar.
+  label = tf.cast(features['label'], tf.int32)
+
+  return image, label
+
+def inputs(train, batch_size, num_epochs):
+  """Reads input data num_epochs times.
+
+  Args:
+    train: Selects between the training (True) and validation (False) data.
+    batch_size: Number of examples per returned batch.
+    num_epochs: Number of times to read the input data, or 0/None to
+       train forever.
+
+  Returns:
+    A tuple (images, labels), where:
+    * images is a float tensor with shape [batch_size, mnist.IMAGE_PIXELS]
+      in the range [-0.5, 0.5].
+    * labels is an int32 tensor with shape [batch_size] with the true label,
+      a number in the range [0, mnist.NUM_CLASSES).
+    Note that an tf.train.QueueRunner is added to the graph, which
+    must be run using e.g. tf.train.start_queue_runners().
+  """
+  if not num_epochs: num_epochs = None
+  filename = os.path.join(FLAGS.train_dir,
+                          TRAIN_FILE if train else VALIDATION_FILE)
+
+  with tf.name_scope('input'):
+    filename_queue = tf.train.string_input_producer(
+        [filename], num_epochs=num_epochs)
+
+    # Even when reading in multiple threads, share the filename
+    # queue.
+    image, label = read_and_decode(filename_queue)
+
+    # Shuffle the examples and collect them into batch_size batches.
+    # (Internally uses a RandomShuffleQueue.)
+    # We run this in two threads to avoid being a bottleneck.
+    images, sparse_labels = tf.train.shuffle_batch(
+        [image, label], batch_size=batch_size, num_threads=2,
+        capacity=1000 + 3 * batch_size,
+        # Ensures a minimum amount of shuffling of examples.
+        min_after_dequeue=1000)
+
+    return images, sparse_labels
+
+def evaluate():
+  """Train MNIST for a number of steps."""
+
+  # Tell TensorFlow that the model will be built into the default Graph.
+  with tf.Graph().as_default():
+    # Input images and labels.
+    images, labels = inputs(train=True, batch_size=FLAGS.batch_size,
+                            num_epochs=FLAGS.num_epochs)
+    # Build a Graph that computes predictions from the inference model.
+    logits = mnist.inference(images,
+                             FLAGS.hidden1,
+                             FLAGS.hidden2)
+
+    top_k_op = tf.nn.in_top_k(logits, labels, 1)
+    saver = tf.train.Saver(tf.trainable_variables())
+
+    saver = tf.train.Saver(tf.trainable_variables())
+    init = tf.initialize_all_variables()
+    # Create a session for running operations in the Graph.
+    sess = tf.Session()
+    sess.run(init)
+    ''' 
+    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+      # Restores from checkpoint
+      saver.restore(sess, ckpt.model_checkpoint_path)
+      # Assuming model_checkpoint_path looks something like:
+      #   /my-favorite-path/cifar10_train/model.ckpt-0,
+      # extract global_step from it.
+      global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+    else:
+      print('No checkpoint file found')
+      return
+    '''
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    step = 0
+    true_count = 0
+    total_count = 0
+    try:
+      step = 0
+      while not coord.should_stop():
+
+        predictions = sess.run([top_k_op])
+        true_count += np.sum(predictions)
+        total_count += 100
+        step += 1
+        sys.exit(-1)
+    except tf.errors.OutOfRangeError:
+      print('Done training for %d epochs, %d steps.' % (FLAGS.num_epochs, step))
+    finally:
+      # When done, ask the threads to stop.
+      coord.request_stop()
+
+    # Wait for threads to finish.
+    coord.join(threads)
+    sess.close()
+
+
+
+
+
+
+def main():
+  evaluate()
+
+
+if __name__ == '__main__':
+  main()
+
