c865d1667912613fc96a0547e75a7093ac193f1d
Muhammad Zaheer <smzaheerabbas@gmail.com>
2016-08-06 20:27:28 +0500

    Added code for early stopping

--- evaluate.py
+++ evaluate.py
@@ -12,30 +12,29 @@ import tensorflow as tf
 
 import mlp
 import input
-
-# Basic model parameters as external flags.
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_integer('num_epochs', 1, 'Number of epochs to run trainer.')
-flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')
-flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
-flags.DEFINE_integer('batch_size', 100, 'Batch size.')
-flags.DEFINE_string('checkpoint_dir', 'checkpoints',
-                           """Directory where to read model checkpoints.""")
 
-def evaluate():
+# Basic model parameters as external flags.
+def eval():
 
   # Tell TensorFlow that the model will be built into the default Graph.
   with tf.Graph().as_default():
+    
+
     # Input images and labels.
     images, labels = input.inputs(train=False, batch_size=FLAGS.batch_size,
-                            num_epochs=FLAGS.num_epochs)
+                            num_epochs=1)
     # Build a Graph that computes predictions from the inference model.
     logits = mlp.inference(images,
                              FLAGS.hidden1,
                              FLAGS.hidden2)
 
+    # Add to the Graph the loss calculation.
+    loss = mlp.loss(logits, labels)
     top_k_op = tf.nn.in_top_k(logits, labels, 1)
+    
+    # To restore the latest checkpoint for evaluation
     saver = tf.train.Saver(tf.trainable_variables())
 
     init = tf.initialize_all_variables()
@@ -46,7 +45,7 @@ def evaluate():
     ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
     if ckpt and ckpt.model_checkpoint_path:
       # Restores from checkpoint
-      print (ckpt.model_checkpoint_path)
+      print ('Evaluating: %s' % ckpt.model_checkpoint_path)
       saver.restore(sess, ckpt.model_checkpoint_path)
       # Assuming model_checkpoint_path looks something like:
       #   /my-favorite-path/cifar10_train/model.ckpt-0,
@@ -61,26 +60,30 @@ def evaluate():
     step = 0
     true_count = 0
     total_count = 0
+    total_loss = 0.
     try:
       step = 0
       while not coord.should_stop():
 
-        predictions = sess.run([top_k_op])
+        predictions, loss_value = sess.run([top_k_op, loss])
         true_count += np.sum(predictions)
         total_count += 100
         step += 1
+        total_loss+=loss_value
     except tf.errors.OutOfRangeError:
-      print('Done training for %d epochs, %d steps.' % (FLAGS.num_epochs, step))
+      print('Evaluation Complete ')
     finally:
       # When done, ask the threads to stop.
       coord.request_stop()
-      print ('Precision: %f' % (true_count/total_count))
+      precision = true_count/total_count
+      print ('Precision: %f Steps: %d' % (precision, step))
     # Wait for threads to finish.
     coord.join(threads)
     sess.close()
+    return total_loss/step
 
 def main():
-  evaluate()
+  eval()
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -6,11 +6,12 @@ import os.path
 import time
 import sys
 
-import numpy
+import numpy as np
 import tensorflow as tf
 
 import mlp
 import input
+import evaluate
 
 # Basic model parameters as external flags.
 flags = tf.app.flags
@@ -53,11 +54,15 @@ def run_training():
     # epoch counter).
     sess.run(init_op)
     step = 0
+    epoch = 0
+
+
     ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
     if ckpt and ckpt.model_checkpoint_path:
       print ('Restoring from checkpoint %s' % ckpt.model_checkpoint_path)
       saver.restore(sess, ckpt.model_checkpoint_path)
       step = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])
+      epoch = step / 65
     else:
       print ('No checkpoint file found - Starting training from scratch')
 
@@ -65,28 +70,54 @@ def run_training():
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-    try:
-      while not coord.should_stop():
-        start_time = time.time()
+    patience = 650 # 3 epochs
+    patience_increase = 2
+    improvement_threshold = 0.995
 
-        # Run one step of the model.  The return values are
-        # the activations from the `train_op` (which is
-        # discarded) and the `loss` op.  To inspect the values
-        # of your ops or variables, you may include them in
-        # the list passed to sess.run() and the value tensors
-        # will be returned in the tuple from the call.
-        _, loss_value = sess.run([train_op, loss])
-        duration = time.time() - start_time
-        step += 1
-        # Print an overview fairly often.
-        if step % 100 == 0:
-          print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value,
+    validation_frequency = 65
+    best_validation_loss = np.inf
+    test_score = 0.
+    done_looping = False
+    epoch = 0
+    n_train_batches = 65 # We have 6500 training examples and batchsize of 100
+
+    try:
+      while not done_looping and not coord.should_stop():
+        epoch = epoch + 1
+        
+        for minibatch_index in range (n_train_batches):
+          start_time = time.time()
+
+          # Run one step of the model.  The return values are
+          # the activations from the `train_op` (which is
+          # discarded) and the `loss` op.  To inspect the values
+          # of your ops or variables, you may include them in
+          # the list passed to sess.run() and the value tensors
+          # will be returned in the tuple from the call.
+          _, loss_value = sess.run([train_op, loss])
+          duration = time.time() - start_time
+          step += 1
+          # Print an overview fairly often.
+          if step % 65 == 0:
+            print('Epoch %d | Step %d: loss = %.2f (%.3f sec)' % (epoch,
+                                                    step, 
+                                                    loss_value,
                                                     duration))
-          checkpoint_path= os.path.join('checkpoints','model.ckpt')
-          saver.save(sess, checkpoint_path,global_step=step)
-          if loss_value <= 4.0:
-            print ('Loss is now less than 4.0 | Terminating')
-            coord.request_stop()
+            checkpoint_path= os.path.join('checkpoints','model.ckpt')
+            saver.save(sess, checkpoint_path,global_step=step)
+            val_loss = evaluate.eval()
+            print ('Validation loss = %.2f' % val_loss)            
+            if val_loss < best_validation_loss:
+              if val_loss < best_validation_loss * improvement_threshold:
+                patience = max(patience, step*patience_increase)
+                best_validation_loss = val_loss
+                saver.save(sess, os.path.join('checkpoints', 'best_model'))
+          if patience <= step:
+            done_looping = True
+            break  
+
+            
+    
     except tf.errors.OutOfRangeError:
       print('Done training for %d epochs, %d steps.' % (FLAGS.num_epochs, step))
     finally:
