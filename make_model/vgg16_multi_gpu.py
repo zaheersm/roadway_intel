46b5e163408e34b4cad34af582ddd81e2a4582d4
Muhammad Zaheer <smzaheerabbas@gmail.com>
2016-09-01 21:58:13 +0500

    Fiddled epochs

--- make_model/vgg16_multi_gpu.py
+++ make_model/vgg16_multi_gpu.py
@@ -24,7 +24,7 @@ LR_DECAY_FACTOR = 0.1
 steps_per_epoch = 1180 # int (82660/70)
 # Factor of 3 since we have a separate minimize for softmax, FC and conv layers
 # Learning rate would be decayed after 3 epochs
-decay_epochs = 30
+decay_epochs = 100
 decay_steps = steps_per_epoch * decay_epochs * 3
 
 def _variable_on_cpu(name, shape, initializer):
